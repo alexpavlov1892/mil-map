89fb400002b2e5a84dfdc793b658140bf34ff6ca
alexpavlov <alexandros.pavlov.2021@uni.strath.ac.uk>
2024-11-21 18:07:46 +0000

    Server and Client setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [backenddata, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch('/api')
+      .then(res => res.json())
+      .then(data => { setBackendData(data)
+      }
+    )
+  }, [])
+
   return (
     <div>
-      <h1>Hello World</h1>
+
+    {(typeof backenddata.users === 'undefined') ? (
+      <p>Loading...</p>
+    ): (
+      backenddata.users.map((user, index) => (
+        <p key={index}>{user}</p>
+      ))
+    )}
+      
     </div>
   )
 }
