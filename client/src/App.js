b5e312213de9b777878d9ec7c14e281b883574df
alexpavlov <alexandros.pavlov.2021@uni.strath.ac.uk>
2024-12-17 11:43:16 +0200

    Late commit...Leaflet.JS initial implementation and starting point fixed to represent Europe

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,37 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react';
+import { MapContainer, TileLayer} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Custom Leaflet marker icons
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+const defaultIcon = L.icon({
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
+L.Marker.prototype.options.icon = defaultIcon;
 
 function App() {
-
-  const [backenddata, setBackendData] = useState([{}])
-
-  useEffect(() => {
-    fetch('/api')
-      .then(res => res.json())
-      .then(data => { setBackendData(data)
-      }
-    )
-  }, [])
-
   return (
-    <div>
-
-    {(typeof backenddata.users === 'undefined') ? (
-      <p>Loading...</p>
-    ): (
-      backenddata.users.map((user, index) => (
-        <p key={index}>{user}</p>
-      ))
-    )}
-      
+    <div style={{ height: '100vh', width: '100%' }}>
+      <MapContainer
+  center={[47.67572320170246, 15.492752747581674]}
+  zoom={5}
+  style={{ height: '100vh', width: '100%' }}
+>
+  <TileLayer
+    attribution='&copy; OpenStreetMap contributors'
+    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+  />
+</MapContainer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/api', (req, res) => {
-    res.json({"users": ["user1", "user2", "user3"] })
+    res.json({"users": ["user1", "user2", "user4"] })
 })
 
 app.listen(5001, () => {console.log("Server is running on port 5001") })
