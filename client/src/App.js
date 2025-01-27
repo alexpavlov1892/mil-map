058d29b2962f4a08a61804cb2d572cf562f0f7cf
alexpavlov <alexandros.pavlov.2021@uni.strath.ac.uk>
2025-01-27 10:55:28 +0000

    Map sizing handled externally by App.js and database in implementation

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MapContainer, TileLayer} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import './App.css';
 
 // Custom Leaflet marker icons
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
@@ -19,18 +20,15 @@ L.Marker.prototype.options.icon = defaultIcon;
 
 function App() {
   return (
-    <div style={{ height: '100vh', width: '100%' }}>
       <MapContainer
   center={[47.67572320170246, 15.492752747581674]}
   zoom={5}
-  style={{ height: '100vh', width: '100%' }}
 >
   <TileLayer
     attribution='&copy; OpenStreetMap contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
 </MapContainer>
-    </div>
   );
 }
 
