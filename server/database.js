4f29f5c0fdc17395a56318034deb6c920c310fae
alexpavlov <alexandros.pavlov.2021@uni.strath.ac.uk>
2025-02-04 11:23:57 +0000

    Database is securely connected and no hard coded passwords

---
+++ server/database.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+    host: process.env.PG_HOST,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE
+});
+
+client.connect()
+    .then(() => console.log('Connected to database'))
+    .catch(err => console.error('Connection error', err.stack));
+
+module.exports = client;
+
+client.query('SELECT * FROM marker', (err, res) => {
+    if(!err){
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end();
+    })
+
